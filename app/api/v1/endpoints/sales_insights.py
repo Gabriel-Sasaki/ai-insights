e4af7ec3f2a0cf3c0a5d63f2c4128b6d7dd79fe3
Gabriel Pinto Sasaki <gabrielpintosasaki@outlook.com>
2025-03-16 21:10:49 -0300

    feat(api): endpoints created

--- app/api/v1/endpoints/sales_insights.py
+++ app/api/v1/endpoints/sales_insights.py
@@ -1,14 +1,44 @@
 from fastapi import Depends, APIRouter
-
 from sqlalchemy.orm import Session
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+from langchain.agents import create_openai_tools_agent
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import SystemMessage
 
 from app.db.session import get_db
+from app.ai.prompts import SQL_QUERY_PROMPT
+from app.ai.models import AgentExecutor
+from app.schemas.sales_insights import SalesInsightsRequest, SalesInsightsResponse
 
 router = APIRouter()
 
 @router.get("/sales-insights")
 async def get_sales_insights(
-    question: str,
+    request: SalesInsightsRequest,
     db: Session = Depends(get_db)
-):
-    pass
+) -> SalesInsightsResponse:
+    """Get sales insights for a given question."""
+    llm = ChatOpenAI(model="gpt-4o")
+    engine = db.get_bind()
+    database = SQLDatabase(engine=engine)
+    toolkit = SQLDatabaseToolkit(db=database, llm=llm)
+    prompt_template = ChatPromptTemplate.from_messages([
+        SystemMessage(content=SQL_QUERY_PROMPT),
+        MessagesPlaceholder("agent_scratchpad"),
+    ])
+    agent = create_openai_tools_agent(
+        llm=llm,
+        tools=toolkit.get_tools(),
+        prompt=prompt_template
+    )
+    agent_executor = AgentExecutor(
+        agent=agent,
+        tools=toolkit.get_tools(),
+        handle_parsing_errors=True
+    )
+    response = agent_executor.invoke({
+        "input": request.question
+    })
+    return SalesInsightsResponse(ai_answer=response["output"])
--- app/api/v1/endpoints/top_products.py
+++ app/api/v1/endpoints/top_products.py
@@ -1,13 +1,30 @@
+import datetime as dt
+
 from fastapi import Depends, APIRouter
 
+from sqlalchemy import select, func
 from sqlalchemy.orm import Session
 
 from app.db.session import get_db
+from app.models.products import Product
+from app.models.sales import Sale
+from app.schemas.top_products import TopProductsResponse
 
 router = APIRouter()
 
 @router.get("/top-products")
-async def get_top_products(
-    db: Session = Depends(get_db)
-):
-    pass
+async def get_top_products(db: Session = Depends(get_db)) -> TopProductsResponse:
+    """Get the top 5 products by sales amount in the current month."""
+    start_of_month = dt.datetime.now().replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+    stmt = select(
+        Product.name,
+        func.sum(Sale.quantity).label("sales_amount")
+    )\
+        .join(Product, Sale.product_id == Product.id)\
+        .group_by(Product.name)\
+        .where(Sale.sale_date >= start_of_month)\
+        .order_by(func.sum(Sale.quantity).desc())\
+        .limit(5)
+    result = db.execute(stmt)
+    top_products = [{"product": prod, "sale_count": sale_count} for prod, sale_count in result]
+    return TopProductsResponse(top_products=top_products)
